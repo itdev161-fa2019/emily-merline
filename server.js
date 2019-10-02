64cf3da416b8d65ce1dde7866af02696d22d824b
Emily Merline <merline1@gmatc.matc.edu>
2019-10-02 18:34:20 -0500

    Created API Endpoint

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ import connectDatabase from './config/db.js';
 
 // Initialize express application
 const app = express();
+
+//Connect database
 connectDatabase();
 
+//Configure Middleware
+app.use(express.json({ extended: false }));
+
 // API endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
