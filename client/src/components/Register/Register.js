f46fa8bd393b0296482bf7362c6faabbe7335276
Emily Merline <merline1@gmatc.matc.edu>
2019-11-06 20:40:51 -0600

    Completed Activity 8

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { userHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const Register = ( { authenticateUser }) => {
     let history = useHistory();
